1098e6b7553fe9a46790a4f996898fd7c8d349ed
Alejandro Rojas <kmachappy@pm.me>
2023-01-09 17:55:55 -0800

    rendered data on list.jsx component

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,17 +1,16 @@
 import { ListItem } from '../components';
 
 export function List({ data }) {
+	console.log(data);
 	return (
 		<>
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
 			<ul>
-				{/**
-				 * TODO: write some JavaScript that renders the `data` array
-				 * using the `ListItem` component that's imported at the top
-				 * of this file.
-				 */}
+				{data.map((item) => {
+					return <ListItem name={item.name} key={item.id} />;
+				})}
 			</ul>
 		</>
 	);
