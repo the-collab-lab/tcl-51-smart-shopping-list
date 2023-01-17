b197b0acd82c77458ec83480bf8a3abb37fe2d71
Amanda DiNoto <amanda.dinoto@gmail.com>
2023-01-16 23:00:15 -0500

    add form, handleSubmit function and onChange function to AddItem.js

--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -1,3 +1,66 @@
-export function AddItem() {
-	return <p>Hello from the <code>/add-item</code> page!</p>
+import { useState } from 'react';
+
+export function AddItem({ addItem }) {
+	const [form, setForm] = useState({ itemName: '', daysUntilNextPurchase: 7 });
+
+	const handleSubmit = (event) => {
+		console.log('Click');
+		event.preventDefault();
+		setForm({ itemName: '', daysUntilNextPurchase: 7 });
+		addItem(form);
+	};
+
+	const onChange = (event) => {
+		setForm({
+			...form,
+			[event.target.name]: event.target.value,
+		});
+	};
+	console.log(form);
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<label htmlFor="itemName">Item Name:</label>
+			<input
+				type="text"
+				id="itemName"
+				name="itemName"
+				value={form.itemName}
+				onChange={onChange}
+			/>
+			<fieldset>
+				<div>
+					<p>How soon will you buy this again?</p>
+				</div>
+				<label htmlFor="soon">Soon</label>
+				<input
+					type="radio"
+					value={7}
+					name="daysUntilNextPurchase"
+					id="soon"
+					onChange={onChange}
+					checked={true}
+				/>
+
+				<label htmlFor="kindOfSoon">Kind of Soon</label>
+				<input
+					type="radio"
+					value={14}
+					name="daysUntilNextPurchase"
+					id="kindOfSoon"
+					onChange={onChange}
+				/>
+
+				<label htmlFor="notSoon">Not Soon</label>
+				<input
+					type="radio"
+					value={30}
+					name="daysUntilNextPurchase"
+					id="notSoon"
+					onChange={onChange}
+				/>
+			</fieldset>
+			<button type="submit">Add Item</button>
+		</form>
+	);
 }
