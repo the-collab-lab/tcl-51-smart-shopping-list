aef1070e51f386a334fb7ff5182de80bb89497dc
Amanda DiNoto <amanda.dinoto@gmail.com>
2023-01-23 20:41:35 -0500

    add conditional button to List,jsx and button clears input when clicked

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -3,17 +3,29 @@ import { ListItem } from '../components';
 
 export function List({ data }) {
 	const [filter, setFilter] = useState('');
+
+	const clickHandler = (event) => {
+		event.preventDefault();
+		setFilter('');
+	};
+
 	return (
 		<>
 			<form>
-				<label htmlFor="itemInput">Filter Items</label>
+				<label htmlFor="itemInput">Filter Items: </label>
 				<input
 					type="text"
 					id="itemInput"
 					name="itemInput"
+					placeholder="Start typing here..."
 					value={filter}
 					onChange={(event) => setFilter(event.target.value)}
 				/>
+				{filter && (
+					<span>
+						<button onClick={clickHandler}>X</button>
+					</span>
+				)}
 			</form>
 			<ul>
 				{data.length > 0 ? (
