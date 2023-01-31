aee944ea209970e79d7051f9b033011602d66247
Aubrey <aubreyfinkelstein@gmail.com>
2023-01-31 14:18:31 -0700

    added function in utils/dates.js to calculate whether dateLastPurchased is within 24 hours of now. Imported this function into ListItem.jsx for future use.

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,7 +1,10 @@
 import { updateItem } from '../api';
+import { withinTwentyFourHours } from '../utils';
 import './ListItem.css';
 
 export function ListItem({ itemData, listToken }) {
+	console.log(withinTwentyFourHours(itemData.dateLastPurchased));
+
 	const onChangeHandler = (event) => {
 		// send listToken (collection), id (document), and value (totalPurchases) to database api
 		updateItem(listToken, event.target.id, Number(event.target.value));
