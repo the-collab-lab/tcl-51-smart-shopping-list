99b8b0f0628b46583982c46aa6d6d5ffcf23790f
Amanda DiNoto <amanda.dinoto@gmail.com>
2023-03-01 00:27:51 -0500

    add styling for form.jsx in index.css

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,49 +1,70 @@
 export default function Form({ handleSubmit, form, onChange }) {
 	return (
-		<>
-			<form onSubmit={handleSubmit} className="">
-				<label htmlFor="itemName">Item Name:</label>
+		<div className="flex flex-col items-center">
+			<form className="pt-5" onSubmit={handleSubmit}>
+				<label htmlFor="itemName" className="ItemLabel">
+					Item Name:
+				</label>
 				<input
 					type="text"
 					id="itemName"
 					name="itemName"
+					className="AddItemInput"
+					placeholder="Enter Item"
 					value={form.itemName}
 					onChange={onChange}
 					required
 				/>
 				<fieldset>
 					<div>
-						<p>How soon will you buy this again?</p>
+						<p className="ItemLabel">How soon will you buy this again?</p>
 					</div>
-					<label htmlFor="soon">Soon</label>
-					<input
-						type="radio"
-						value={7}
-						name="daysUntilNextPurchase"
-						id="soon"
-						onChange={onChange}
-						checked={form.daysUntilNextPurchase === 7}
-					/>
+					<div className="RadioItem item-short">
+						<input
+							type="radio"
+							value={7}
+							name="daysUntilNextPurchase"
+							id="soon"
+							className="RadioInput"
+							onChange={onChange}
+							checked={form.daysUntilNextPurchase === 7}
+						/>
+						<label className="ml-2" htmlFor="soon">
+							Soon
+						</label>
+					</div>
 
-					<label htmlFor="kindOfSoon">Kind of Soon</label>
-					<input
-						type="radio"
-						value={14}
-						name="daysUntilNextPurchase"
-						id="kindOfSoon"
-						onChange={onChange}
-					/>
+					<div className="RadioItem item-mid">
+						<input
+							type="radio"
+							value={14}
+							name="daysUntilNextPurchase"
+							id="kindOfSoon"
+							className="RadioInput"
+							onChange={onChange}
+						/>
+						<label className="ml-2" htmlFor="kindOfSoon">
+							Kind of Soon
+						</label>
+					</div>
 
-					<label htmlFor="notSoon">Not Soon</label>
-					<input
-						type="radio"
-						value={30}
-						name="daysUntilNextPurchase"
-						id="notSoon"
-						onChange={onChange}
-					/>
+					<div className="RadioItem item-long">
+						<input
+							type="radio"
+							value={30}
+							name="daysUntilNextPurchase"
+							id="notSoon"
+							className="RadioInput"
+							onChange={onChange}
+						/>
+						<label className="ml-2" htmlFor="notSoon">
+							Not Soon
+						</label>
+					</div>
 				</fieldset>
-				<button type="submit">Add Item</button>
+				<button className="FormButton" type="submit">
+					Add Item
+				</button>
 			</form>
 
 			{form.isSubmitted && (
@@ -52,6 +73,6 @@ export default function Form({ handleSubmit, form, onChange }) {
 					{form.isSuccess ? 'Item added Successfully' : form.error}
 				</div>
 			)}
-		</>
+		</div>
 	);
 }
