51cdc24f5f12041dafc0805b0c42c346922e07c4
aayush@nexleaf.org <aayush@nexleaf.org>
2024-01-07 17:05:39 -0800

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -3,12 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: 'AIzaSyCX-vM3JkbLC5dSBBeDQAgmmGEKU_269gY',
-	authDomain: 'tcl-51-smart-shopping-list.firebaseapp.com',
-	projectId: 'tcl-51-smart-shopping-list',
-	storageBucket: 'tcl-51-smart-shopping-list.appspot.com',
-	messagingSenderId: '970974663458',
-	appId: '1:970974663458:web:c60c278055491604890b7b',
+	apiKey: 'AIzaSyBcrT6-36uNMEM_MsFJhp_0ka7Cwx-Gag0',
+	authDomain: 'tcl-51-smart-shopping-li-31ec4.firebaseapp.com',
+	projectId: 'tcl-51-smart-shopping-li-31ec4',
+	storageBucket: 'tcl-51-smart-shopping-li-31ec4.appspot.com',
+	messagingSenderId: '32212467526',
+	appId: '1:32212467526:web:aaa2ceb4eff7a3f469af47',
+	measurementId: 'G-D0E0NWCE8Q',
 };
 
 // Initialize Firebase
